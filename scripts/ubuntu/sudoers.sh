f9c54d91dda49daa19f22240337d0314aeec948b
advancevillage <cugriver@gmail.com>
2017-10-06 15:39:28 +0800

    0.1.0

--- scripts/ubuntu/sudoers.sh
+++ scripts/ubuntu/sudoers.sh
@@ -5,3 +5,12 @@ sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers;
 # Set up password-less sudo for the vagrant user
 echo 'vagrant ALL=(ALL) NOPASSWD:ALL' >/etc/sudoers.d/vagrant;
 chmod 440 /etc/sudoers.d/vagrant;
+
+webuser=cuger
+webpasswd=richardkelly
+groupadd $webuser
+useradd -c $webuser -d /home/$webuser -g $webuser -s /bin/bash  $webuser
+echo $webuser:$webpasswd | chpasswd
+echo "$webuser ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$webuser
+chmod 440 /etc/sudoers.d/$webuser
+
